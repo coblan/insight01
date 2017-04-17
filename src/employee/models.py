d1ffe87baabd83aa0eddf4f7dd040b3ffd01b011
heyulin <heyulin@hotmail.com>
2017-04-17 21:09:17 +0800

    start

---
+++ src/employee/models.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User,Group
+from django.db import models
+from django.utils.translation import ugettext as _
+
+# Create your models here.
+
+class EmployeeModel(models.Model):
+    user = models.ForeignKey(User,verbose_name=_('user'), blank=True, null=True)
+    baseinfo=models.OneToOneField('BasicInfo',verbose_name=_('basic info'),blank=True,null=True)
+   
+    def __unicode__(self):
+        if self.baseinfo:
+            return self.baseinfo.name
+        else:
+            return self.pk    
+
+class BasicInfo(models.Model):
+    name = models.CharField(_('name'), max_length=50, blank=True)
+    age = models.CharField(_('age'), max_length=50, blank=True)
+    head = models.CharField(_('head image'),max_length=200,blank=True)
+    id_number=models.CharField(_('id  number'),max_length=200,blank=True)
+    address=models.CharField(_('address'),max_length=500,blank=True)
+    gen = models.CharField(_('gen'),max_length=30,blank=True)
+    phone = models.CharField(_('phone'),max_length=100,blank=True)
+    
+    
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name=_('basic info')
---
+++ src/settings/dev.py
@@ -0,0 +1,28 @@
+from base import *
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+#MEDIA_ROOT= os.path.join( os.path.dirname(BASE_DIR),'media')
+
+#MIDDLEWARE_CLASSES =('helpers.maintenance.request_log.RequestMiddleware',)+MIDDLEWARE_CLASSES 
+
+TEMPLATE_DEBUG = True
+
+
+import os
+if os.environ.get('TEST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
+    }  
+else:
+    DATABASES = {
+        'default': {
+            'NAME': 'insight01',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': '127.0.0.1', 
+            'PORT': '3306',        
+          },
+        }
