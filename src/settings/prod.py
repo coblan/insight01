4e19ad37dadbbd1afe0c9c2c35e08ac27a5823d8
heyulin <coblan@163.com>
2017-07-02 12:53:11 +0800

    sg

--- src/settings/prod.py
+++ src/settings/prod.py
@@ -4,7 +4,27 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 from helpers.maintenance.log import log_setting
 log_setting.SET(globals())
 
-TEMPLATE_DEBUG = True
+#TEMPLATE_DEBUG = True
+
+DEBUG=False
+ALLOWED_HOSTS=['i01.enjoyst.com']
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+    ('coblan', 'coblan@163.com'),
+)
+
+MANAGERS = (
+    ('coblan', 'coblan@163.com'),
+)
+
+EMAIL_HOST='smtp.163.com'
+EMAIL_HOST_PASSWORD='he7125158'
+EMAIL_HOST_USER='coblan'
+EMAIL_SUBJECT_PREFIX='[INSIGHT01]'
+SERVER_EMAIL='coblan@163.com'
+DEFAULT_FROM_EMAIL='coblan@163.com'
+
 
 
 DATABASES = {
